184db14cdb89612cb6df1d0d2f88194cce7b9213
xm-tech <maxm09@163.com>
2023-03-03 19:21:54 +0800

    test enum

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+enum Conf {
+	WIDTH = 500, 
+	HEIGHT = 300
+};
+
 int main() {
 	printf("welcome to the c game world \n");
+	printf("width:%d,height:%d\n", WIDTH, HEIGHT);
 	return 0;
 }
